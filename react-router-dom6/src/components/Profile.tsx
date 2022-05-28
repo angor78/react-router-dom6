4d9896fc729e382cab751218d1cb44dcf742fde2
Angor78 <angor78@gmail.com>
2022-05-28 15:48:42 +0300

    useNavigate

--- react-router-dom6/src/components/Profile.tsx
+++ react-router-dom6/src/components/Profile.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 export const Profile = () => {
+  const navigate = useNavigate()
 
   return(
     <div>
+      
       <h2>Profile</h2>
+      <button onClick={()=>navigate('/login')}>logout</button>
     </div>
   )
 }
