3804ff741a081c87ca6d60bb6398e458c968cb97
Angor78 <angor78@gmail.com>
2022-05-28 14:24:36 +0300

    add componets:main,login,profile

--- react-router-dom6/src/App.tsx
+++ react-router-dom6/src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes} from "react-router-dom";
+import {Main} from "./components/Main";
+import {Login} from "./components/Login";
+import {Profile} from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>React-router-dom6</h1>
+        <NavLink to={'/'}>main</NavLink>
+        <NavLink to={'/login'}>login</NavLink>
+        <NavLink to={'/profile'}>profile</NavLink>
+
+        <Routes>
+          <Route path={'/'} element={<Main/>}/>
+          <Route path={'/login'} element={<Login/>}/>
+          <Route path={'/profile'} element={<Profile/>}/>
+        </Routes>
       </header>
     </div>
   );
